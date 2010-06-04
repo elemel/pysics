b271a0f909fedf212b2f38c3a3c55533d268c28f
Mikael Lind <elemel@elemel.se>
2010-06-04 22:18:15 +0200

    finished initial wrapping of b2Body

--- src/test_pysics.py
+++ src/test_pysics.py
@@ -1,7 +1,37 @@
-from pysics_box2d import *
+from pysics import *
+
+def test_create_world():
+    world = World(Vec2(0.0, -10.0), True)
+
+def test_body_types_are_distinct():
+    body_types = [STATIC_BODY, KINEMATIC_BODY, DYNAMIC_BODY]
+    assert len(body_types) == len(set(body_types))
+
+def test_user_data_of_body_keeps_identity():
+    world = World(Vec2(0.0, -10.0), True)
+    body_def = BodyDef()
+    body = world.create_body(body_def)
+    obj = object()
+    obj_repr = repr(obj)
+    body.user_data = obj
+    del obj
+    assert repr(body.user_data) == obj_repr
+
+def test_body_can_have_none_as_user_data():
+    world = World(Vec2(0.0, -10.0), True)
+    body_def = BodyDef()
+    body = world.create_body(body_def)
+    assert body.user_data is None
+    body.user_data = object()
+    assert body.user_data is not None
+    body.user_data = None
+    assert body.user_data is None
 
 def main():
-    world = World(Vec2(0.0, -10.0), True)
+    test_create_world()
+    test_body_types_are_distinct()
+    test_user_data_of_body_keeps_identity()
+    test_body_can_have_none_as_user_data()
 
 if __name__ == '__main__':
     main()
