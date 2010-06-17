50ce04c2aded0ce797f97bbea255dee3ae7926e3
Mikael Lind <elemel@elemel.se>
2010-06-17 22:30:28 +0200

    Got the unit tests working again. Trying a setup/teardown scheme using contextlib.

--- src/pysics/test_pysics.py
+++ src/pysics/test_pysics.py
@@ -1,45 +1,44 @@
+from contextlib import contextmanager
+import nose
 from pysics import *
 
-def test_create_world():
+@contextmanager
+def manage_world():
     world = World(Vec2(0, -10), True)
+    yield world
+
+@contextmanager
+def manage_body():
+    with manage_world() as world:
+        body = world.create_body()
+        yield body
+        world.destroy_body(body)
 
 def test_body_types_are_distinct():
     body_types = [STATIC_BODY, KINEMATIC_BODY, DYNAMIC_BODY]
     assert len(body_types) == len(set(body_types))
 
 def test_user_data_of_body_keeps_identity():
-    world = World(Vec2(0, -10), True)
-    body_def = BodyDef()
-    body = world.create_body(body_def)
-    obj = object()
-    obj_repr = repr(obj)
-    body.user_data = obj
-    del obj
-    assert repr(body.user_data) == obj_repr
+    with manage_body() as body:
+        obj = object()
+        obj_repr = repr(obj)
+        body.user_data = obj
+        del obj
+        assert repr(body.user_data) == obj_repr
 
 def test_body_can_have_none_as_user_data():
-    world = World(Vec2(0, -10), True)
-    body_def = BodyDef()
-    body = world.create_body(body_def)
-    assert body.user_data is None
-    body.user_data = object()
-    assert body.user_data is not None
-    body.user_data = None
-    assert body.user_data is None
+    with manage_body() as body:
+        assert body.user_data is None
+        body.user_data = object()
+        assert body.user_data is not None
+        body.user_data = None
+        assert body.user_data is None
 
 def test_vec_2_has_length_2():
     assert len(Vec2(0, 0)) == 2
 
-def test_vec_2_is_iterable_with_length_2():
+def test_vec_2_unpacks_with_length_2():
     x, y = Vec2(0, 0)
 
-def main():
-    test_create_world()
-    test_body_types_are_distinct()
-    test_user_data_of_body_keeps_identity()
-    test_body_can_have_none_as_user_data()
-    test_vec_2_has_length_2()
-    test_vec_2_is_iterable_with_length_2()
-
 if __name__ == '__main__':
-    main()
+    nose.main()
