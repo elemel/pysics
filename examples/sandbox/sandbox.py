5c06446272f5928c615fcda990f4bd3d89a8ba76
Mikael Lind <elemel@elemel.se>
2010-06-26 00:05:44 +0200

    Sandbox: use Pinky to load scene from SVG

--- examples/sandbox/sandbox.py
+++ examples/sandbox/sandbox.py
@@ -1,8 +1,10 @@
 from contextlib import contextmanager
 import math
+import pinky
 import pyglet
 from pyglet.gl import *
 import pysics
+import sys
 
 def iter_links(link):
     while link is not None:
@@ -68,15 +70,92 @@ def draw_vertices(vertices, mode):
         for x, y in vertices:
             glVertex2f(x, y)
 
+def load_document(document, world):
+    width = float(document.root.attributes['width'])
+    height = float(document.root.attributes['height'])
+    matrix = pinky.Matrix.create_scale(0.01, -0.01) * pinky.Matrix.create_translate(-0.5 * width, -0.5 * height)
+    load_element(document.root, matrix, {}, world, None)
+
+def parse_body_type(body_type_str):
+    if body_type_str == 'static':
+        return pysics.STATIC_BODY
+    elif body_type_str == 'kinematic':
+        return pysics.KINEMATIC_BODY
+    elif body_type_str == 'dynamic':
+        return pysics.DYNAMIC_BODY
+    else:
+        raise ValueError('invalid body type: %s' % body_type_str)
+
+def parse_float_tuple(float_tuple_str):
+    return tuple(float(s) for s in float_tuple_str.replace(',', ' ').split())
+
+def parse_bool(bool_str):
+    if bool_str == 'false':
+        return False
+    elif bool_str == 'true':
+        return True
+    else:
+        raise ValueError('invalid boolean: %s' % bool_str)
+
+def parse_body_attributes(attributes):
+    return dict(type=parse_body_type(attributes.get('body-type', 'static')),
+                linear_velocity=parse_float_tuple(attributes.get('linear-velocity', '0 0')),
+                angular_velocity = float(attributes.get('angular-velocity', '0')),
+                linear_damping = float(attributes.get('linear-damping', '0')),
+                angular_damping = float(attributes.get('angular-damping', '0')),
+                allow_sleep = parse_bool(attributes.get('allow-sleep', 'true')),
+                awake = parse_bool(attributes.get('awake', 'true')),
+                fixed_rotation = parse_bool(attributes.get('fixed-rotation', 'false')),
+                bullet = parse_bool(attributes.get('bullet', 'false')),
+                active = parse_bool(attributes.get('active', 'true')),
+                inertia_scale = float(attributes.get('inertia-scale', '1')))
+
+def parse_shape_attributes(attributes):
+    return dict(friction=float(attributes.get('friction', '0.2')),
+                restitution=float(attributes.get('restitution', '0')),
+                density=float(attributes.get('density', '1')),
+                sensor=parse_bool(attributes.get('sensor', 'false')),
+                category_bits=int(attributes.get('category-bits', '0001'), 16),
+                mask_bits=int(attributes.get('mask-bits', 'ffff'), 16),
+                group_index=int(attributes.get('group-index', '0')))
+
+def load_element(element, matrix, attributes, world, body):
+    matrix = matrix * element.matrix
+    local_attributes = dict(element.attributes)
+    for attribute in ('style', 'desc'):
+        local_attributes.update(pinky.parse_style(local_attributes.pop(attribute, '')))
+    attributes = dict(attributes)
+    attributes.update(local_attributes)
+    type_ = local_attributes.get('type')
+    if body is None and (type_ == 'body' or element.shape is not None):
+        body_attributes = parse_body_attributes(attributes)
+        body = world.create_body(**body_attributes)
+    if element.shape is not None:
+        transformed_shape = element.shape.transform(matrix)
+        shape_attributes = parse_shape_attributes(attributes)
+        if isinstance(transformed_shape, pinky.Circle):
+            body.create_circle_fixture(position=(transformed_shape.cx,
+                                                 transformed_shape.cy),
+                                       radius=transformed_shape.r,
+                                       **shape_attributes)
+        elif isinstance(transformed_shape, pinky.Polygon):
+            if transformed_shape.area >= 0.0:
+                vertices = transformed_shape.points
+            else:
+                vertices = list(reversed(transformed_shape.points))
+            body.create_polygon_fixture(vertices=vertices, **shape_attributes)
+    for child in element.children:
+        load_element(child, matrix, attributes, world, body)
+
 class MyWindow(pyglet.window.Window):
-    def __init__(self, **kwargs):
+    def __init__(self, documents, **kwargs):
         super(MyWindow, self).__init__(**kwargs)
         self.screen_time = 0.0
         self.world_time = 0.0
         self.world_dt = 1.0 / 60.0
         self.world = pysics.World((0.0, -10.0), True)
-        self.world.create_kinematic_body(angular_velocity=-0.5).create_circle_fixture(radius=1.5)
-        self.world.create_dynamic_body(position=(0.0, 5.0), angular_velocity=1.0).create_box_fixture()
+        for document in documents:
+            load_document(document, self.world)
         self.clock_display = pyglet.clock.ClockDisplay()
         pyglet.clock.schedule_interval(self.step, 0.1 * self.world_dt)
 
@@ -92,13 +171,16 @@ class MyWindow(pyglet.window.Window):
 
     def on_draw(self):
         self.clear()
-        with manage_screen_transform(self.width, self.height, 0.1):
+        with manage_screen_transform(self.width, self.height, 0.05):
             for body in iter_links(self.world.body_list):
                 draw_body(body)
         self.clock_display.draw()
 
 def main():
-    window = MyWindow(fullscreen=True)
+    documents = [pinky.Document(arg) for arg in sys.argv[1:]]
+    config = pyglet.gl.Config(double_buffer=True, sample_buffers=1, samples=4,
+                              depth_size=8)
+    window = MyWindow(documents, fullscreen=True, config=config)
     pyglet.app.run()
 
 if __name__ == '__main__':
