bd9c65461e93e1aa1e4d4a50ef0551eee762cefb
Mikael Lind <elemel@elemel.se>
2010-07-04 20:53:07 +0200

    cleaned up unit test

--- src/pysics/test_pysics.py
+++ src/pysics/test_pysics.py
@@ -8,45 +8,47 @@ def manage_world():
     yield world
 
 @contextmanager
-def manage_body(world=None):
-    if world is None:
-        with manage_world() as world:
-            body = world.create_static_body()
-            yield body
-            world.destroy_body(body)
-    else:
-        body = world.create_static_body()
-        yield body
-        world.destroy_body(body)
+def manage_static_body(world, *args, **kwargs):
+    body = world.create_static_body(*args, **kwargs)
+    yield body
+    world.destroy_body(body)
 
 @contextmanager
-def manage_circle_fixture():
-    with manage_body() as body:
-        circle_fixture = body.create_circle_fixture()
-        yield circle_fixture
-        body.destroy_fixture(circle_fixture)
+def manage_kinematic_body(world, *args, **kwargs):
+    body = world.create_kinematic_body(*args, **kwargs)
+    yield body
+    world.destroy_body(body)
 
 @contextmanager
-def manage_edge_fixture():
-    with manage_body() as body:
-        edge_fixture = body.create_edge_fixture()
-        yield edge_fixture
-        body.destroy_fixture(edge_fixture)
+def manage_dynamic_body(world, *args, **kwargs):
+    body = world.create_dynamic_body(*args, **kwargs)
+    yield body
+    world.destroy_body(body)
 
 @contextmanager
-def manage_polygon_fixture():
-    with manage_body() as body:
-        polygon_fixture = body.create_polygon_fixture()
-        yield polygon_fixture
-        body.destroy_fixture(polygon_fixture)
+def manage_circle_fixture(body, *args, **kwargs):
+    fixture = body.create_circle_fixture(*args, **kwargs)
+    yield fixture
+    body.destroy_fixture(fixture)
 
 @contextmanager
-def manage_loop_fixture():
-    with manage_body() as body:
-        vertex_array = VertexArray()
-        loop_fixture = body.create_loop_fixture(vertex_array)
-        yield loop_fixture
-        body.destroy_fixture(loop_fixture)
+def manage_edge_fixture(body, *args, **kwargs):
+    fixture = body.create_edge_fixture(*args, **kwargs)
+    yield fixture
+    body.destroy_fixture(fixture)
+
+@contextmanager
+def manage_polygon_fixture(body, *args, **kwargs):
+    fixture = body.create_polygon_fixture(*args, **kwargs)
+    yield fixture
+    body.destroy_fixture(fixture)
+
+@contextmanager
+def manage_loop_fixture(body, *args, **kwargs):
+    vertex_array = VertexArray()
+    fixture = body.create_loop_fixture(vertex_array, *args, **kwargs)
+    yield fixture
+    body.destroy_fixture(fixture)
 
 @contextmanager
 def manage_revolute_joint(world, *args, **kwargs):
@@ -66,56 +68,65 @@ def manage_distance_joint(world, *args, **kwargs):
     yield distance_joint
     world.destroy_joint(distance_joint)
 
-def test_body_types_are_distinct():
-    body_types = [STATIC_BODY, KINEMATIC_BODY, DYNAMIC_BODY]
-    assert len(body_types) == len(set(body_types))
-
-def test_user_data_of_body_keeps_identity():
-    with manage_body() as body:
-        obj = object()
-        obj_repr = repr(obj)
-        body.user_data = obj
-        del obj
-        assert repr(body.user_data) == obj_repr
-
-def test_body_can_have_none_as_user_data():
-    with manage_body() as body:
-        assert body.user_data is None
-        body.user_data = object()
-        assert body.user_data is not None
-        body.user_data = None
-        assert body.user_data is None
-
 def test_exercise():
-    with manage_circle_fixture() as fixture:
-        pass
-    with manage_edge_fixture() as fixture:
-        pass
-    with manage_polygon_fixture() as fixture:
-        pass
-    with manage_loop_fixture() as fixture:
-        pass
+    with manage_world() as world:
+        with manage_dynamic_body(world) as body:
+            with manage_circle_fixture(body) as fixture:
+                pass
+            with manage_edge_fixture(body) as fixture:
+                pass
+            with manage_polygon_fixture(body) as fixture:
+                pass
+            with manage_loop_fixture(body) as fixture:
+                pass
 
 def test_create_revolute_joint():
     with manage_world() as world:
-        with manage_body(world) as body_a:
-            with manage_body(world) as body_b:
+        with manage_static_body(world) as body_a:
+            with manage_dynamic_body(world) as body_b:
                 with manage_revolute_joint(world, body_a, body_b, (0, 0)) as revolute_joint:
                     pass
 
 def test_create_prismatic_joint():
     with manage_world() as world:
-        with manage_body(world) as body_a:
-            with manage_body(world) as body_b:
+        with manage_static_body(world) as body_a:
+            with manage_dynamic_body(world) as body_b:
                 with manage_prismatic_joint(world, body_a, body_b, (0, 0), (0, 0)) as prismatic_joint:
                     pass
 
 def test_create_distance_joint():
     with manage_world() as world:
-        with manage_body(world) as body_a:
-            with manage_body(world) as body_b:
+        with manage_static_body(world) as body_a:
+            with manage_dynamic_body(world) as body_b:
                 with manage_distance_joint(world, body_a, body_b, (0, 0), (0, 0)) as distance_joint:
                     pass
+
+def _test_identity(a, b):
+    assert a is not b
+    assert id(a) != id(b)
+    assert a == b
+    assert not a != b
+    assert hash(a) == hash(b)
+
+def test_body_identity():
+    with manage_world() as world:
+        with manage_dynamic_body(world):
+            _test_identity(world.body_list, world.body_list)
+
+def test_fixture_identity():
+    with manage_world() as world:
+        with manage_dynamic_body(world) as body:
+            with manage_circle_fixture(body):
+                _test_identity(body.fixture_list, body.fixture_list)
+
+"""
+def test_joint_identity():
+    with manage_world() as world:
+        with manage_static_body(world) as body_a:
+            with manage_dynamic_body(world) as body_b:
+                with manage_revolute_joint(world, body_a, body_b, (0, 0)):
+                    print body_a.joint_list
+"""
 
 if __name__ == '__main__':
     nose.main()
